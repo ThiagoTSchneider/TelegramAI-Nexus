4183e59166e36574a31077011f70f8fca67e9700
Thiago Schneider <76605923+ThiagoTSchneider@users.noreply.github.com>
2023-09-02 01:18:37 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import telebot
+import asyncio
 import os
 import aiogram
 
@@ -14,18 +15,19 @@ voice_model = "Adam"
 Telegram_API = Telegram_api
 
 asis = telebot.TeleBot(Telegram_API, parse_mode='HTML')
-print('Asis: {✓} Successfully Connected to Telegram API')
+print('{✓} Successfully Connected to Telegram API\n')
 
 
 def generate_and_send_audio(chat_id, response):
-  print("Initializing Audio Generation\n")
+  print(" ⚠ Initializing Audio Generation")
   audio = generate(text=response,
                    voice=voice_model,
                    model='eleven_multilingual_v1')
   save(audio, "Asis.ogg")
   voice = open('Asis.ogg', 'rb')
   asis.send_voice(chat_id, voice,)
-  print("Audio Generated, Asis is Free for New Requests\n")
+  print("{✓} Audio Generated\n")
+  print("-> Asis is Free for New Requests <-")
 
 
 @asis.message_handler(func=lambda message: True)
@@ -33,8 +35,8 @@ def handle_all_messages(message):
   user_input = message.text
   response = comp(user_input)
   asis.send_message(message.chat.id, response)
+  print('{✓} Successfully Generated Response\n')
   generate_and_send_audio(message.chat.id, response)
-  print('{✓} AsisAutoCompletion Done')
 
 
 asis.polling()

4bd08014a468544cb2a6c0faaa6688d104b0e82d
Thiago Schneider <76605923+ThiagoTSchneider@users.noreply.github.com>
2023-09-03 17:06:00 -0300

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ set_api_key(elevenlabs_key)
 voice_model = "Adam"
 Telegram_API = Telegram_api
 
-asis = telebot.TeleBot(Telegram_API, parse_mode='HTML')
+bot = telebot.TeleBot(Telegram_API, parse_mode='HTML')
 print('{✓} Successfully Connected to Telegram API\n')
 
 
@@ -25,18 +25,18 @@ def generate_and_send_audio(chat_id, response):
                    model='eleven_multilingual_v1')
   save(audio, "Asis.ogg")
   voice = open('Asis.ogg', 'rb')
-  asis.send_voice(chat_id, voice,)
+  bot.send_voice(chat_id, voice,)
   print("{✓} Audio Generated\n")
-  print("-> Asis is Free for New Requests <-")
+  print("-> Bot is Free for New Requests <-")
 
 
-@asis.message_handler(func=lambda message: True)
+@bot.message_handler(func=lambda message: True)
 def handle_all_messages(message):
   user_input = message.text
   response = comp(user_input)
-  asis.send_message(message.chat.id, response)
+  bot.send_message(message.chat.id, response)
   print('{✓} Successfully Generated Response\n')
   generate_and_send_audio(message.chat.id, response)
 
 
-asis.polling()
+bot.polling()

903cd6b00995b3b2d136b9a23cc2d0439fae002c
Thiago Schneider <76605923+ThiagoTSchneider@users.noreply.github.com>
2023-09-03 17:06:37 -0300

    Update gpt.py

--- gpt.py
+++ gpt.py
@@ -6,14 +6,14 @@ openai.api_key = openai_api
 
 def comp(user_input):
   global generated_response
-  print(" ⚠ User Requested Asis AutoCompletion Response")
+  print(" ⚠ User Requested Bot AutoCompletion Response")
   response = openai.ChatCompletion.create(
       model="gpt-3.5-turbo",
       messages=[{
           "role":
           "system",
           "content":
-          "Voce é uma inteligencia artifical,que imita o comportamento humano,replicando sentimentos em suas falas,e conversas,usa bons emojis para se expressar,e sempre responde de um jeito atencioso para as perguntas,seu nome é Asis."
+          "Voce é uma inteligencia artifical,que imita o comportamento humano,replicando sentimentos em suas falas,e conversas,usa bons emojis para se expressar,e sempre responde de um jeito atencioso para as perguntas,seu nome é Bot."
       }, {
           "role": "user",
           "content": user_input
